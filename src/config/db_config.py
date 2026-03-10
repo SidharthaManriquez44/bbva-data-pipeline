4e632f4a68d981feb3c05f742b1f75f8248c59cf
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-10 11:42:45 -0600

    feat(pipeline) src(db_config)

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,14 +1,6 @@
-import os
-from sqlalchemy import create_engine
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 def get_engine():
-    user = os.environ["DB_USER"]
-    password = os.environ["DB_PASSWORD"]
-    host = os.environ["DB_HOST"]
-    port = os.environ["DB_PORT"]
-    db = os.environ["DB_NAME"]
-
-    return create_engine(
-        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}", pool_size=5, max_overflow=10
-    )
+    hook = PostgresHook(postgres_conn_id="banking_dw")
+    return hook.get_sqlalchemy_engine()
