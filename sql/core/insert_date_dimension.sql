9605d3820bc9594a92e69808ad456c72d3ee94cd
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-09 21:48:38 -0600

    update(pipeline) sql(.sqlfluff)

--- sql/core/insert_date_dimension.sql
+++ sql/core/insert_date_dimension.sql
@@ -12,7 +12,10 @@ INSERT INTO core.dim_date (
     is_weekend,
     is_month_end,
     is_quarter_end,
-    is_year_end
+    is_year_end,
+    month_start_date,
+    quarter_start_date,
+    year_start_date
 )
 SELECT
     TO_CHAR(d, 'YYYYMMDD')::INT AS date_key,
@@ -25,10 +28,13 @@ SELECT
     TO_CHAR(d, 'Day') AS day_name,
     EXTRACT(WEEK FROM d)::SMALLINT AS week_of_year,
     EXTRACT(ISODOW FROM d)::SMALLINT AS day_of_week,
-    EXTRACT(ISODOW FROM d) IN (6, 7) AS is_weekend,
+    EXTRACT(ISODOW FROM d) IN (6,7) AS is_weekend,
     (DATE_TRUNC('month', d) + INTERVAL '1 month - 1 day')::DATE = d AS is_month_end,
     (DATE_TRUNC('quarter', d) + INTERVAL '3 month - 1 day')::DATE = d AS is_quarter_end,
-    (DATE_TRUNC('year', d) + INTERVAL '1 year - 1 day')::DATE = d AS is_year_end
+    (DATE_TRUNC('year', d) + INTERVAL '1 year - 1 day')::DATE = d AS is_year_end,
+    DATE_TRUNC('month', d)::DATE AS month_start_date,
+    DATE_TRUNC('quarter', d)::DATE AS quarter_start_date,
+    DATE_TRUNC('year', d)::DATE AS year_start_date
 FROM GENERATE_SERIES(
     '2000-01-01'::DATE,
     '2035-12-31'::DATE,
