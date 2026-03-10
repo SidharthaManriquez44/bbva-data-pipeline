ea517ffafd4d33e647069b5e6cffc412691006b8
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-10 14:33:33 -0600

    update(pipeline) test(db_config, db_local)

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,6 +1,5 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-
-
 def get_engine():
+    from airflow.providers.postgres.hooks.postgres import PostgresHook
+
     hook = PostgresHook(postgres_conn_id="banking_dw")
     return hook.get_sqlalchemy_engine()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from src.config.db_config import get_engine
+from src.config.db_local import get_engine
 
 
 @pytest.fixture
@@ -32,3 +32,21 @@ def fake_data():
         }
     )
     return fake_df
+
+
+@pytest.fixture
+def raw_df():
+    raw_df = pd.DataFrame(
+        {
+            "bank_code": ["BBVA", "BBVA", "BBVA"],
+            "year": ["2020", "2021", 2022],
+            "branches": [1745, "1716", 1732],
+            "atms": [12950, "13400", 14019],
+            "total_clients": [27500000, "28000000", 29500000],
+            "digital_clients": [20000000, 21000000, "22000000"],
+            "total_loans": [1450000000000, 1550000000000, "1650000000000"],
+            "total_deposits": ["1500000000000", 1600000000000, 1680000000000],
+            "net_income": [680000000000, 750000000000, "800000000000"],
+        }
+    )
+    return raw_df
