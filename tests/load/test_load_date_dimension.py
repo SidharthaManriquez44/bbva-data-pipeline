9aef98d366c37c41fe44f0763af046394dfa26c1
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-12 11:38:42 -0600

    feat(pipeline): tests(date dimension)

--- tests/load/test_load_date_dimension.py
+++ tests/load/test_load_date_dimension.py
@@ -1,15 +1,15 @@
 from unittest.mock import patch, MagicMock
-from src.load.channel_dimension import load_dim_channel
+from src.load.date_dimension import load_dim_date
 
 
 # Test 1: Using an external connection
-@patch("src.load.channel_dimension.load_sql")
-def test_load_dim_channel_with_connection(mock_load_sql):
+@patch("src.load.date_dimension.load_sql")
+def test_load_dim_date_with_connection(mock_load_sql):
     mock_load_sql.return_value = "SELECT 1"
 
     fake_conn = MagicMock()
 
-    load_dim_channel(fake_conn)
+    load_dim_date(fake_conn)
 
     mock_load_sql.assert_called_once()
 
@@ -17,9 +17,9 @@ def test_load_dim_channel_with_connection(mock_load_sql):
 
 
 # Test 2 — offline (creates engine)
-@patch("src.load.channel_dimension.load_sql")
-@patch("src.load.channel_dimension.get_engine")
-def test_load_dim_channel_create_engine(mock_get_engine, mock_load_sql):
+@patch("src.load.date_dimension.load_sql")
+@patch("src.load.date_dimension.get_engine")
+def test_load_dim_date_create_engine(mock_get_engine, mock_load_sql):
     mock_load_sql.return_value = "SELECT 1"
 
     fake_conn = MagicMock()
@@ -28,7 +28,7 @@ def test_load_dim_channel_create_engine(mock_get_engine, mock_load_sql):
     mock_get_engine.return_value = fake_engine
     fake_engine.begin.return_value.__enter__.return_value = fake_conn
 
-    load_dim_channel()
+    load_dim_date()
 
     mock_get_engine.assert_called_once()
 
