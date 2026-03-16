e6e37996eb4d7312f737222b3b07e678f99853fb
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-16 10:20:25 -0600

    update(pipeline): test(db local)

--- tests/integration/test_db_local.py
+++ tests/integration/test_db_local.py
@@ -1,15 +1,16 @@
 import pytest
-from sqlalchemy import text
-from src.config.db_local import get_engine
+from sqlalchemy import create_engine, text
+from testcontainers.postgres import PostgresContainer
 
 
 @pytest.mark.integration
 def test_postgres_connection():
-    engine = get_engine()
+    with PostgresContainer("postgres:16") as postgres:
+        engine = create_engine(postgres.get_connection_url())
 
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT version()"))
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT version()"))
 
-    version = result.scalar()
+        version = result.scalar()
 
-    assert "PostgreSQL" in version
+        assert "PostgreSQL" in version
